293303d15afc26dc30b13184878e96e58dc5c840
Rachel Kurchin <rkurchin@mit.edu>
2019-12-27 13:50:20 -0500

    first commit

---
+++ cv.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import pybtex.database.input.bibtex
+from cv_fcns import *
+
+# name formats in .bib file (to make bold in publications section)
+name_formats = ["R. Kurchin", "R. C. Kurchin"]
+
+# define file paths to content
+pubs_fpath = "MyPapers.bib"
+awards_fpath = "../rkurchin.github.io/markdown_generator/awards.tsv"
+talks_fpath = "../rkurchin.github.io/markdown_generator/talks.tsv"
+posters_fpath = "../rkurchin.github.io/markdown_generator/posters.tsv"
+service_fpath = "../rkurchin.github.io/markdown_generator/service.tsv"
+
+# define paths to TeX outputs
+pubs_texpath = "inputs/pubs.tex"
+awards_texpath = "inputs/awards.tex"
+talks_texpath = "inputs/talks.tex"
+posters_texpath = "inputs/posters.tex"
+service_texpath = "inputs/service.tex"
+
+# make dictionaries for easy indexing/readability
+# (there's probably a tidier way to do this...)
+sections = ["awards", "talks", "posters", "service"]
+sections_files = {"awards":awards_fpath, "talks":talks_fpath, "posters":posters_fpath, "service":service_fpath}
+sections_tex = {"awards":awards_texpath, "talks":talks_texpath, "posters":posters_texpath, "service":service_texpath}
+sections_fcns = {"awards":list_award, "talks":list_pres, "posters": list_pres} # add service
+
+# write out the TeX files
+for sec in sections[:-1]:
+    # read in the data
+    datafile = sections_files[sec]
+    info = pd.read_csv(datafile, sep="\t", header=0)
+    texfile = sections_tex[sec]
+    list_func = sections_fcns[sec]
+
+    with open(texfile,'w') as f:
+        # list first one explicitly
+        list_func(dict(info.iloc[0]), f)
+        year = info.iloc[0].date[:4]
+        # then iterate through and print year only when it changes
+        for row, item in info.iloc[1:].iterrows():
+            if item.date[:4]==year:
+                list_func(dict(item), f, include_year=False)
+            else:
+                year = item.date[:4]
+                list_func(dict(item), f)
+
+            
+#### PUBLICATIONS
+# doing separately from other sections for now because of the numbering
+# could probably all be rolled in without *too* much difficulty
+
+# read in data
+parser = pybtex.database.input.bibtex.Parser()
+pubs = parser.parse_file(pubs_fpath)
+
+# write TeX file
+with open(pubs_texpath,'w') as f:
+    for n,entry in enumerate(pubs.entries):
+        m = len(pubs.entries)-n # reverse numbering
+
+        if len(str(m)) == 1: # pad number
+            num_str = '0' + str(m)
+        else:
+            num_str = str(m)
+
+        # for first one, include year
+        if n==0:
+            list_pub(pubs, entry, f, num_str, name_formats)
+            year = pubs.entries[entry].fields['year']
+        else:
+            if pubs.entries[entry].fields['year'] == year:
+                list_pub(pubs, entry, f, num_str, name_formats, include_year=False)
+            else:
+                year = pubs.entries[entry].fields['year']
+                list_pub(pubs, entry, f, num_str, name_formats)
---
+++ cv_fcns.py
@@ -0,0 +1,87 @@
+
+### functions for generating strings of TeX code
+def cvItem_text(date, inst, dept, descr):
+    return '\cvItem{{{date}}}{{{inst}}}{{{dept}}}{{{descr}}}\n'.format(date=date, inst=inst, dept=dept, descr=descr)
+
+def pubsItem_text(date, num, text):
+    return '\pubsItem{{{date}}}{{{num}}}{{{text}}}\n'.format(date=date, num=num, text=text)
+
+# I was using this for dates on talks/posters before I decided to just put years for everything
+#def format_date(date_str):
+#    (year, month, day) = date_str.split('-')
+#    return month+'/'+day+'/'+year
+
+### functions for formatting particular item types
+# awards/fellowships
+def list_award(award_info, f, include_year=True):
+    if include_year:
+        year = str(award_info['year'])
+    else:
+        year = ""
+    f.write(cvItem_text(year, award_info['title'], award_info['granter'], ''))
+
+# publication
+def list_pub(pubs, entry, f, num, name_formats, include_year=True):
+    if include_year:
+        year = pubs.entries[entry].fields['year']
+    else:
+        year = ""
+    f.write(pubsItem_text(year, num, get_citation(pubs, entry, name_formats)))
+
+# presentations (talk/poster)
+def list_pres(pres_info, f, include_year=True):
+    if include_year:
+        year = str(pres_info['date'][:4])
+    else:
+        year = ""
+    # scooches title back a bit to line up with next line
+    f.write(cvItem_text(year, '', "\hspace{-1.5mm}"+pres_info['title'], pres_info['venue']))
+
+
+# citation stuff
+def get_citation(pubs, entry, to_bold):
+    # original code for this function from Giuseppe Romano
+
+    data = pubs.entries[entry]
+
+    # author list
+    n = len(data.persons['author'])
+    strc = ''
+    for i,au in enumerate(data.persons['author']):
+
+        tmp = str(au).split(',')
+        if tmp[1].count(' ') == 2:
+            middle_name = ' ' + tmp[1][-1] + '. '
+        else:
+            middle_name = ' '
+
+        name = tmp[1].strip()[0]
+        last_name = tmp[0]
+        author = name + '.' + middle_name + last_name
+
+        if author in to_bold:
+            author = "\\textbf{{{a}}}".format(a=author)
+
+        if i < n-1:
+            strc += author
+            strc += ', '
+        else:
+            strc += author
+            strc += '. '
+
+    # everything else
+    if 'url' in data.fields.keys():
+        url = data.fields['url']
+    else:
+        url = ''
+
+    strc += r'''``''' + data.fields['title'] + r'''.'' '''
+    for w in data.fields['journal'].split():
+        strc += '\href{' + url + '}{' + '\\textit{' + w + '}' + '} '
+    if len(data.fields['volume'])> 0:
+        strc += '\href{''' + url + '}{' + str(data.fields['volume']) + '}, '
+    if len(data.fields['pages'])> 0:
+        strc += r'''\href{''' + url + r'''}{''' + str(data.fields['pages']) + '} '
+        strc += r'''\href{''' + url + r'''}{(''' + str(data.fields['year']) + ')}'
+
+    return strc
